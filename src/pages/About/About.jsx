9136eae122a6e13ed8efaa0808209cd981a69ed2
pcheung <pika206@gmail.com>
2022-10-14 07:55:54 -0500

    imp first draft about section

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,6 +2,10 @@ function About(){
   return (  
     <>
     <h1>About</h1>
+    <a href="https://imgur.com/rp5M5ah"><img src="https://i.imgur.com/rp5M5ahb.jpg" title="source: imgur.com" alt="headshot of Patrick Cheung" /></a>
+    <p>Full Stack Web Developer with proven product management experience with business SaaS organizations.  Leads Agile Scrum teams, and uses modern web technologies such as React.</p>
+
+    <p>Other Interests include spending time with his family in San Antonio, bass fishing, bass guitar, and the occasional home improvement project.</p> 
     </>
   )
 }
