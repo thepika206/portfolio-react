242513725900294fc703903e8ba76da898e45464
pcheung <pika206@gmail.com>
2022-10-13 20:09:32 -0500

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Projects from './pages/Projects/Projects';
+import Resume from './pages/Resume/Resume';
 import './App.css';
+
 
 function App() {
   return (
-    <>  
-      <p>more content to come</p>
-    </>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='/projects' element={<Projects />} />
+      <Route path='/resume' element={<Resume />} />
+    </Routes>
   );
 }
 
