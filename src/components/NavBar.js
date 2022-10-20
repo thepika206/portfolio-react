edc033b9616fa6f692d18296981b4695c0d7fdcd
pcheung <pika206@gmail.com>
2022-10-20 07:24:58 -0500

    fix ref to class instead of classname

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,7 +9,7 @@ function NavBar(){
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
+      <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item nav-link">
             <Link to='/' >Home</Link>
