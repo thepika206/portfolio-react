57ddabfa5036f70d5bf252adfabf73f7fbcd067b
pcheung <pika206@gmail.com>
2022-10-27 12:29:04 -0500

    add basic information for Plane

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,10 +1,18 @@
 const projectsData = [
   {
+    title: "Plan.e",
+    description: "A Traveler's app for Planning Trips",
+    technologies: "MERN Stack: MongoDB, Express.js, React.js, Node.js",
+    image: "https://i.imgur.com/wnxMtXl.png",
+    repositoryLink: "https://github.com/thepika206/sales-tracker",
+    deploymentLink: "https://wifi-travelers-plane.netlify.app/",
+  },
+  {
     title: "Sales Tracker",
     description: "A mobile friendly CRM for Sales Agents",
     technologies: "MongoDB, Mongoose, Express.js, Node.js, EJS templates",
     image: "https://i.imgur.com/wnxMtXl.png",
-    repositoryLink: "https://github.com/thepika206/sales-tracker",
+    repositoryLink: "https://github.com/thepika206/plane-front-end",
     deploymentLink: "https://sales-tracker-pcheung.fly.dev/",
   },
   {
