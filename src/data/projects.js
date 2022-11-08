a008db3ce291e7ece0a91931b71f5a3fa80fcd9a
pcheung <pika206@gmail.com>
2022-11-07 19:47:29 -0600

    update technologies on projects to reduce repetition

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,9 +1,9 @@
 const projectsData = [
   {
     title: "Plan.e",
-    description: "A travel app - full stack with a decoupled backend and JWT Auth",
+    description: "A travel app - full stack React App",
     otherContributors: "Group-project with Jennifer Juarez, Chris Elliot, and Naveen Bhatti",
-    technologies: "MERN Stack: MongoDB, Express.js, React.js, Node.js",
+    technologies: "React.js, JWT Auth",
     image: "https://i.imgur.com/lMq2ujy.png",
     image2: "https://i.imgur.com/TMy6WdR.png",
     repositoryLink: "https://github.com/thepika206/sales-tracker",
@@ -11,9 +11,9 @@ const projectsData = [
   },
   {
     title: "Sales Tracker",
-    description: "Full CRUD app featuring Google OAuth",
+    description: "Full CRUD app",
     
-    technologies: "MongoDB, Mongoose, Express.js, Node.js, EJS templates",
+    technologies: "MongoDB/Mongoose, Express.js, EJS templates, BootStrap, Google OAuth",
     image: "https://i.imgur.com/u3lBr6V.png",
     repositoryLink: "https://github.com/thepika206/plane-front-end",
     deploymentLink: "https://sales-tracker-pcheung.fly.dev/",
@@ -22,7 +22,7 @@ const projectsData = [
     title: "BlackJack",
     description: "An fun mobile friendly card game",
     
-    technologies: "JavaScript, HTML, CSS",
+    technologies: "JavaScript",
     image: "https://i.imgur.com/3qzc0Tz.png",
     repositoryLink: "https://github.com/thepika206/play-blackjack",
     deploymentLink: "https://thepika206-play-blackjack.netlify.app/",
