1fb02eb2ae9722d09780593b885325dc7bc8cd9d
pcheung <pika206@gmail.com>
2022-11-09 10:57:45 -0600

    Add takeaways to Project data and preview

--- src/components/ProjectPreview.js
+++ src/components/ProjectPreview.js
@@ -6,22 +6,20 @@ const ProjectPreview = ({project}) => {
   console.log(projectUrl)
   return (  
     <div className="">
-        <div className="card mb-2">
-          <a href={projectUrl}>
-            <img src={project.image} alt={project.title} className="project-thumbnail card-image p-2"/>
-          </a>
+      <div className="card mb-2">
+        <a href={projectUrl}>
+          <img src={project.image} alt={project.title} className="project-thumbnail card-image p-2"/>
+        </a>
         <div className="card-body">
           <h3>{project.title}</h3>
-          <h6>{project.technologies}</h6>
-        <a href={'/projects/' + hyphenateWords(project.title)}>
-          <button className="btn btn-outline-dark mb-3"> Learn More </button>
-        </a>
-          
-        </div>
-
-        
+          <p className="fw-bold">Key Takeaways: {project.takeaways}</p>
+          <p>Description: {project.description}</p>
+          <a href={'/projects/' + hyphenateWords(project.title)}>
+            <button className="btn btn-outline-dark mb-3"> Learn More </button>
+          </a>
         </div>
       </div>
+    </div>
   );
 } 
 export default ProjectPreview
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,9 +1,21 @@
 const projectsData = [
+  // {
+  //   title: "Bass Collector",
+  //   description: "An collection app for keeping track of your bass-related musician gear",
+    
+  //   technologies: "Django, Python",
+  //   takeaways: "Learned Python and Django framework for rapid feature development using Class Based Views and built-in auth",
+  //   image: "",
+  //   image2: "",
+  //   repositoryLink: "https://github.com/thepika206/bass-collector",
+  //   deploymentLink: "",
+  // },
   {
     title: "Plan.e",
-    description: "A travel app - full stack React App",
+    description: "A travel app that lets you plan a trip and share ideas for what to do",
     otherContributors: "Group-project with Jennifer Juarez, Chris Elliot, and Naveen Bhatti",
     technologies: "React.js, JWT Auth",
+    takeaways: "Working with React, Group Development",
     image: "https://i.imgur.com/lMq2ujy.png",
     image2: "https://i.imgur.com/TMy6WdR.png",
     repositoryLink: "https://github.com/thepika206/sales-tracker",
@@ -11,9 +23,10 @@ const projectsData = [
   },
   {
     title: "Sales Tracker",
-    description: "Full CRUD app",
+    description: "Full CRM app for tracking clients",
     
     technologies: "MongoDB/Mongoose, Express.js, EJS templates, BootStrap, Google OAuth",
+    takeaways: "Node and Express framework, Full CRUD functionality",
     image: "https://i.imgur.com/u3lBr6V.png",
     repositoryLink: "https://github.com/thepika206/plane-front-end",
     deploymentLink: "https://sales-tracker-pcheung.fly.dev/",
@@ -23,6 +36,7 @@ const projectsData = [
     description: "An fun mobile friendly card game",
     
     technologies: "JavaScript",
+    takeaways: "Flow Control for Game Logic, DOM manipulation",
     image: "https://i.imgur.com/3qzc0Tz.png",
     repositoryLink: "https://github.com/thepika206/play-blackjack",
     deploymentLink: "https://thepika206-play-blackjack.netlify.app/",
