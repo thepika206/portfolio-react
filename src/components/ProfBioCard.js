74c2de4656fc20df6a88388632b2e331132a8aea
pcheung <pika206@gmail.com>
2022-11-12 17:37:07 -0600

    update about page

--- src/components/ProfBioCard.js
+++ src/components/ProfBioCard.js
@@ -2,8 +2,12 @@ const ProfBioCard = () => {
   return (  
     <div className="card m-3">
       <div className="card-body">
-        <h3 className="card-title">From Product Owner to Developer...</h3>
-        <p>After many years of Product Management, Patrick found his favorite part of the job was spent investigating code with fellow developers.  After a period of self-teaching, in 2022 he initiated a formal career switch into Software Development by entering a full-time bootcamp at General Assembly.</p> 
+        <h3 className="card-title">PO Experience</h3>
+        <ul>
+          <li>Real-World Software Knowledge</li>
+          <li>Functional Testing and Design Experience</li>
+          <li>Intensely Curious</li>
+        </ul> 
         <a href="/projects">
             <button className="btn btn-dark">View Projects</button>
         </a>
