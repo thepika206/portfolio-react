0cd58e03bb87446d89ce334dbcc1950f6434e1ee
pcheung <pika206@gmail.com>
2022-11-18 08:40:18 -0600

    update home page txt

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,19 +7,19 @@ function Home(){
       <h2>Software Developer</h2>
       <img id="home-image" className="mb-3 img-fluid" src="https://i.imgur.com/hcGYNrO.jpg" title="source: imgur.com" alt="headshot of Patrick Cheung" />
       {/* <h4>An Engineer with Product Experience</h4> */}
+      <a href="/about">
+        <button className="btn btn-dark">Learn More</button>
+      </a>
       <div className="card m-3" id="home-card">
           <div className="card-body">
             <h3 className="card-title">An Engineer with Product Experience</h3>
             <ul>
-              <li className="fs-5">10 years experience in the Software Industry</li>
-              <li className="fs-5">Transitioned from Product Management to Engineering</li>
-              <li className="fs-5">A Whole Product Minded Approach</li>
+              <li className="fs-5">Skilled in JavaScript, React.js, Node.js</li>
+              <li className="fs-5">Experience with Agile planning, UI Design and Testing</li>
+              <li className="fs-5">Industry expertise: Insurance, HR, Lead Marketing</li>
             </ul>
           </div>
         </div>
-      <a href="/about">
-        <button className="btn btn-dark">Learn More</button>
-      </a>
     </>
   )
 }
