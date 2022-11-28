39d58b1338deec5334b43ecc18cdccb716d73f86
pcheung <pika206@gmail.com>
2022-11-28 16:06:23 -0600

    update home page text

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -2,9 +2,12 @@ const HomeCard = () => {
   return (  
     <div className="card p-2" id="home-card">
       <div className="card-body">
-        <h3 className="card-title mb-3">Full Stack  Developer</h3>
-        <h5>Motivated Web Developer | Product and Project Management | Customer Collaborator </h5>
-        
+        <h3 className="card-title mb-3">Full Stack Developer</h3>
+        <h5>I am a Software Engineer with a Product Owner background</h5>
+        <ul>
+          <li>React | Javascript | Python</li>
+          <li>RESTful APIs | Node.js | Express </li>
+        </ul>
       </div>
     </div>
   );
