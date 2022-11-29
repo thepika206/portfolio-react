67289d13eaff01de8a4b6af402b2c28407ead14f
pcheung <pika206@gmail.com>
2022-11-28 20:02:10 -0600

    fix mobile layout on home

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -2,11 +2,12 @@ const HomeCard = () => {
   return (  
     <div className="card p-2" id="home-card">
       <div className="card-body">
-        <h3 className="card-title mb-3">Full Stack Developer</h3>
-        <h5>I am a Software Engineer with a Product Owner background</h5>
+
+        <h5>Software Engineer</h5>
         <ul>
           <li>React | Javascript | Python</li>
-          <li>RESTful APIs | Node.js | Express </li>
+          <li>RESTful APIs | Node.js | Express</li>
+          <li>Agile Product Owner experience</li>
         </ul>
       </div>
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,19 @@
 import HomeCard from "../../components/HomeCard"
+import './home.css'
 
 function Home(){
   return (  
     <>
-      <h1>
+      <h1 id='home-title'>
         Patrick Cheung
       </h1>
       {/* <h2>Software Developer</h2> */}
       
-      <div className="d-flex justify-content-center flex-sm-wrap">
-        <img id="home-image" className="mb-3" src="https://i.imgur.com/hcGYNrO.jpg" title="source: imgur.com" alt="headshot of Patrick Cheung" />
-        {/* <h4>An Engineer with Product Experience</h4> */}
+      <div id="home-container">
+        <div id='home-image-container'>
+          <img id="home-image" className="mb-3" src="https://i.imgur.com/hcGYNrO.jpg" title="source: imgur.com" alt="headshot of Patrick Cheung" />
+          {/* <h4>An Engineer with Product Experience</h4> */}
+        </div>
         <HomeCard/>  
       </div>
       <a href="/about">
