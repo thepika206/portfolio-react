4d4372f3815c401cd268e2218ca9ccbd9e18fa4b
pcheung <pika206@gmail.com>
2022-12-02 17:30:10 -0600

    update branding statement

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -3,7 +3,7 @@ const HomeCard = () => {
     <div className="card p-2 home-card" id="home-card">
       <div className="card-body">
         <h4>
-          I am a full stack developer with a product owner background.  By collaborating with clients and stakeholders, I solve business solutions using modern technology and teamwork.
+          I am a full stack developer with a product owner background.  I solve business solutions using modern technology and teamwork across the organization.
         </h4>
       </div>
     </div>
