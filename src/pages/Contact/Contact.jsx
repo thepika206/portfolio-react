df23aa2d25be681c8859d3c15be7acffe00a91ef
pcheung <pika206@gmail.com>
2022-12-05 15:53:29 -0600

    imp icons for contact links

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,31 +1,41 @@
+import './contact.css'
+
 function Contact(){
   return (  
     <>
       <h1>Contact</h1>
       <div className="card m-3 contact-card">
         <div className="card-body">
-          <h3 className="card-title">Information</h3>
+          <h3 className="card-title">Connect with Me</h3>
           <ul>
               <li>
-                <a href="mailto:pika206@gmail.com">pika206@gmail.com</a>
+                <a href="mailto:pika206@gmail.com">
+                  <img className="icon" src="images/icons/icons8-mail-50.png" alt="email" />
+                  pika206@gmail.com
+                </a>
                 </li>
               <li>
-                <a href="https://www.linkedin.com/in/patrick-cheung206/">LinkedIn</a>
+                <a href="https://www.linkedin.com/in/patrick-cheung206/">
+                  <img className="icon" src="images/icons/LI-In-Bug.png" alt="LinkedIn" />
+                </a>
               </li>
               <li>
-                <a href="https://github.com/thepika206">GitHub</a>
+                <a href="https://github.com/thepika206">
+                  <img className="icon" src="images/icons/github-mark.png" alt="GitHub" />
+                </a>
               </li>
               <li>
-                <a href="/resume/Resume-Patrick-Cheung.pdf" download>Download Resume</a>
+                <img src="https://www.codewars.com/users/thepika206/badges/small
+                " alt="codewars badge" />
               </li>
             </ul>
-            <img src="https://www.codewars.com/users/thepika206/badges/small" alt="codewars badge" />
+            
         </div>
       </div>      
       <div className="card m-3 contact-card">
         <div className="card-body">
           <h3 className="card-title">Personal Life</h3>
-          <p>Patrick Cheung is originally from Seattle, Washington, and now lives in San Antonio, Texas.</p> 
+          <p>I live in San Antonio, Texas and enjoy spending time with my family, woodworking and playing music.</p> 
         </div>
       </div>
     </>
