9ff370384c0c655297b04adab48bde929724106d
pcheung <pika206@gmail.com>
2022-12-07 15:01:10 -0600

    home page layout change

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,19 +5,19 @@ import TechSkillsCard from "../../components/TechSkillsCard"
 function Home(){
   return (  
     <>
-      <h1 id='home-title'>
-      👋👋👋 I'm Patrick Cheung
-      </h1>
-      <a href="/projects">
-          <button className="mb-3 btn btn-dark">View Projects</button>
-      </a>
       <div id="home-container">
-        <div className="card home-card" id='home-image-container'>
-          <img id="home-image" className="" src="https://i.imgur.com/hcGYNrO.jpg" title="source: imgur.com" alt="headshot of Patrick Cheung" />
+        <div className="m-3" id='home-image-container'>
+          <img id="home-image" className="" src="https://i.imgur.com/rp5M5ah.jpg" title="source: imgur.com" alt="headshot of Patrick Cheung" />
         </div>
+        <div id="home-title" className="m-3">
+          <h1 id='home-title'>👋 I'm Patrick Cheung</h1>
+          <h3>Full Stack Developer</h3>
+        </div>
+        <a href="/projects">
+          <button className="mb-3 btn btn-dark">View Projects</button>
+        </a>
         <TechSkillsCard/>
-        <HomeCard/>
-          
+        <HomeCard/>  
       </div>
       <a href="/projects">
           <button className="mb-3 btn btn-dark">View Projects</button>
