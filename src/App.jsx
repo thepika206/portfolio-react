7b2a8b6a96e2aaaba9448317ef650a7fe7f3ec71
pcheung <pika206@gmail.com>
2022-12-09 10:44:27 -0600

    implement random chuck norris joke on contact page

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,30 @@ import Resume from './pages/Resume/Resume';
 import NavBar from './components/NavBar';
 import ProjectDetails from './pages/ProjectDetails/ProjectDetails';
 import { workExperienceData } from './data/workExperience';
+import { getRandomJoke } from './services/chuckNorrisService';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [joke, setJoke] = useState({})
+
+  useEffect(() => {
+    const fetchRandomJokeData = async() => {
+      const randomJokeData = await getRandomJoke()
+      setJoke(randomJokeData)
+    }
+    fetchRandomJokeData()
+  },[])
+
   return (
     <>
       <NavBar />
         <main className='d-flex flex-column  align-items-center'>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/contact' element={<Contact />} />
+        <Route path='/' element={<Home 
+        />} />
+        <Route path='/contact' element={<Contact 
+          joke = {joke}
+        />} />
         <Route path='/projects' element={<Projects />} />
         <Route path='/resume' element={<Resume 
           workExperience={workExperienceData}
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import './contact.css'
 
-function Contact(){
+function Contact(props){
   return (  
     <>
       <h1>Contact</h1>
@@ -36,6 +36,13 @@ function Contact(){
         <div className="card-body">
           <h3 className="card-title">Personal Life</h3>
           <p>I live in San Antonio, Texas and enjoy spending time with my family, woodworking and playing music.</p> 
+        </div>
+      </div>
+      <div className="card p-4 m-5 contact-card">
+        <div className="card-body">
+          <h3 className="card-title">Just for Fun: Random Chuck Norris Joke</h3>
+          <p>{props.joke.value}</p>
+          <p>Credit: <a href="https://api.chucknorris.io/">https://api.chucknorris.io/</a></p> 
         </div>
       </div>
     </>
