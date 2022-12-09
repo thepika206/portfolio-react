4cf2bcd1c087b70c9914cd5af089325284dd0e9f
pcheung <pika206@gmail.com>
2022-12-09 13:25:02 -0600

    conditionally render the joke card only if joke is fetched

---
+++ src/components/JokeCard.js
@@ -0,0 +1,18 @@
+const JokeCard = (props) => {
+  return (  
+    <>
+      <div className="card p-4 m-5 contact-card">
+          <div className="card-body">
+            <h3 className="card-title">Just for Fun: Random Chuck Norris Joke</h3>
+            <p>{props.joke?.value}</p>
+            <p>Credit: <a href="https://api.chucknorris.io/">https://api.chucknorris.io/</a></p> 
+          </div>
+        </div>
+    </>
+    
+    
+  
+  )
+}
+
+export default JokeCard;
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,4 +1,5 @@
 import './contact.css'
+import JokeCard from '../../components/JokeCard'
 
 function Contact(props){
   return (  
@@ -8,28 +9,27 @@ function Contact(props){
         <div className="card-body">
           <h3 className="card-title">Connect with Me</h3>
           <ul>
-              <li>
-                <a href="mailto:pika206@gmail.com">
-                  <img className="icon" src="images/icons/icons8-mail-50.png" alt="email" />
-                  pika206@gmail.com
-                </a>
-                </li>
-              <li>
-                <a href="https://www.linkedin.com/in/patrick-cheung206/">
-                  <img className="icon" src="images/icons/LI-In-Bug.png" alt="LinkedIn" />
-                </a>
+            <li>
+              <a href="mailto:pika206@gmail.com">
+                <img className="icon" src="images/icons/icons8-mail-50.png" alt="email" />
+                pika206@gmail.com
+              </a>
               </li>
-              <li>
-                <a href="https://github.com/thepika206">
-                  <img className="icon" src="images/icons/github-mark.png" alt="GitHub" />
-                </a>
-              </li>
-              <li>
-                <img src="https://www.codewars.com/users/thepika206/badges/small
-                " alt="codewars badge" />
-              </li>
-            </ul>
-            
+            <li>
+              <a href="https://www.linkedin.com/in/patrick-cheung206/">
+                <img className="icon" src="images/icons/LI-In-Bug.png" alt="LinkedIn" />
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/thepika206">
+                <img className="icon" src="images/icons/github-mark.png" alt="GitHub" />
+              </a>
+            </li>
+            <li>
+              <img src="https://www.codewars.com/users/thepika206/badges/small
+              " alt="codewars badge" />
+            </li>
+          </ul>
         </div>
       </div>      
       <div className="card p-4 m-5 contact-card">
@@ -38,13 +38,11 @@ function Contact(props){
           <p>I live in San Antonio, Texas and enjoy spending time with my family, woodworking and playing music.</p> 
         </div>
       </div>
-      <div className="card p-4 m-5 contact-card">
-        <div className="card-body">
-          <h3 className="card-title">Just for Fun: Random Chuck Norris Joke</h3>
-          <p>{props.joke.value}</p>
-          <p>Credit: <a href="https://api.chucknorris.io/">https://api.chucknorris.io/</a></p> 
-        </div>
-      </div>
+      {
+        props.joke
+        ? <JokeCard joke={props.joke} />
+        :<></>
+      }
     </>
   )
 }
