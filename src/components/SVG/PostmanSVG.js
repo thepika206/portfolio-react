19b6c853e8e2f30dfe3ac107868590935acc4eda
pcheung <pika206@gmail.com>
2022-12-13 09:34:54 -0600

    refactor technologies svgs into the data file

--- src/components/SVG/PostmanSVG.js
+++
@@ -1,7 +0,0 @@
-const PostmanSVG = () => {
-  return (  
-    <img alt="Postman icon" src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/48/null/external-postman-is-the-only-complete-api-development-environment-logo-color-tal-revivo.png"/>
-  );
-}
- 
-export default PostmanSVG;
--- src/components/TechSkillsCard.js
+++ src/components/TechSkillsCard.js
@@ -1,39 +1,13 @@
-// import { techSkillsData } from "../data/techSkills";
-import CSSSVG from "./SVG/CSSSVG";
-import DjangoSVG from "./SVG/DjangoSVG";
-import ExpressSVG from "./SVG/ExpressSVG";
-import GithubSVG from "./SVG/GithubSVG";
-import HTML5 from "./SVG/HTMLSVG";
-import JavaScriptSVG from "./SVG/JavaScriptSVG";
-import MongoSVG from "./SVG/MongoSVG";
-import NodeSVG from "./SVG/NodeSVG";
-import PostgresSVG from "./SVG/PostgresSVG";
-import PostmanSVG from "./SVG/PostmanSVG";
-import PythonSVG from "./SVG/PythonSVG";
-import ReactSVG from "./SVG/ReactSVG";
-import TypeScriptSVG from "./SVG/TypeScriptSVG";
+import { techSkillsData } from "../data/techSkills";
 const TechSkillsCard = () => {
   return (  
     <div className="card home-card p-3 m-5 shadow" >
       <div className="card-body">
         <h3 className="card-title">Technologies</h3>
-        <JavaScriptSVG/>
-        <ReactSVG/>
-        <TypeScriptSVG/>
-        <HTML5/>
-        <CSSSVG/>
-        <PythonSVG/>
-        <ExpressSVG/>
-        <DjangoSVG/>
-        <NodeSVG/>
-        <MongoSVG/>
-        <PostgresSVG/>
-        <PostmanSVG/>
-        <GithubSVG/>
-        {/* <ul>{techSkillsData.map(skill =>
-            <li className="fs-5" key={skill.title}>{skill.title}</li>
+        <div className="flex-row-container">{techSkillsData.map(skill =>
+            <div className="" key={skill.title}>{skill.svg}</div>
           )}
-        </ul> */}
+        </div>
       </div>
       <div className="card-footer text-muted">
         <a href="https://icons8.com/license">used with permission from icons8.com</a>
