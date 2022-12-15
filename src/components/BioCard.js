dfde47fdd5a19062ad54ea82c8790f4925fad2b6
pcheung <pika206@gmail.com>
2022-12-15 17:28:13 -0600

    update bio

--- src/components/BioCard.js
+++ src/components/BioCard.js
@@ -4,10 +4,10 @@ const BioCard = () => {
       <div className="card-body">
         <h2 className="card-title text-center mb-3">Professional Journey</h2>
         <p>
-          My career in tech started in Portland Oregon with supporting Insurance CRM, training, and implementing clients.  I gained a mastery and love for the business of software.  Then I took a role on the Product team and continued contributing to future capabilities of the software platform.  
+          My career in tech started in Portland Oregon with supporting Insurance CRM, training, and implementing clients.  I gained expertise in the application and a love for the business of software.  Then I took a role on the Product team and continued contributing to the development of new features.  
         </p>
         <p>
-          As a Product Owner, I exercised skills in agile project management, feature design, functional testing, and release management.   Later I continued to level up in UI/UX, product packaging and strategy, and handling all forms of communications.  Through it all, my highest appreciation was for engineers who produced the software. This led me to consider a major career change towards software engineering.
+          As a Product Owner, I exercised skills in agile project management, feature design, functional testing, and release management.   Later I continued to level up in UI/UX, product packaging, strategy, and release management.  Of all the elements of the job, my favorite was the time spent working shoulder-to-shoulder with developers. This led me to consider a career change to software engineering.
         </p> 
         <p>
           To validate my intentions in this career pivot, I interviewed several developers that I have worked with to gain additional perspective and brainstorm ideas for coding camps. During this time I began self-paced courses in web development in order to fully confirm that I would enjoy the challenge of coding. After 6 weeks of working full-time and teaching myself JavaScript basics at night, I felt confident to take the leap into Software Development.
