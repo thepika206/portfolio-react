e86c94d529247e3a0d92f69f3886090c13cd58f2
pcheung <pika206@gmail.com>
2023-08-12 22:38:23 -0500

    rewrote bio

--- src/components/BioCard.js
+++ src/components/BioCard.js
@@ -4,10 +4,13 @@ const BioCard = () => {
       <div className="card-body">
         <h2 className="card-title text-center mb-3">Professional Journey</h2>
         <p>
-          Currently a junior developer at inquirED, a maker of high quality inquiry-based social studies curriculum, making regular code contributions, reviewing PRs, an building out the testing foundations. I am honored to be part of a small but mighty team of engineers, as I level up in code. 
+          I am a junior developer at inquirED, where I am learning the ropes of software development while contributing to the creation of high-quality inquiry-based social studies curriculum. I am passionate about using technology to make education more engaging and accessible, and I am excited to be a part of a team that is making a difference in the world.
         </p>
         <p>
-          Prior to this, I have served as a Product Owner on several teams in across multiple industries managing Enterprise software projects.  I have industry knowledge in Insurance, Lead Management, and Corporate Ethics and Compliance.
+          In my previous role as a product owner, I managed enterprise software projects across multiple industries. I have a deep understanding of the product development lifecycle, and I am skilled at working with cross-functional teams to deliver successful products. I am also an expert in user experience design and usability testing.
+        </p>
+        <p>
+          I am a highly motivated and results-oriented individual with a strong work ethic. I am also a team player and I am always willing seek out better solutions and processes for how the work is done. I am confident that I have the skills and experience to be a valuable asset to any team.
         </p>
       </div>
     </div>
