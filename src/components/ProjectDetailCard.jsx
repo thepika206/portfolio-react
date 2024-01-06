29beca1d0c46e65c7dd00037b7155770c9f8ff90
pcheung <pika206@gmail.com>
2024-01-06 15:29:59 -0600

    update projects deployment links and format ternaries

--- src/components/ProjectDetailCard.jsx
+++ src/components/ProjectDetailCard.jsx
@@ -2,27 +2,22 @@
 
 const ProjectDetailCard = ({project}) => {
   return (  
-    <>
-      <div className="card mb-2 m-5 shadow">
-        <div className="card-body">
-          <h5>{project.description}</h5>
-          <ul>
-            <li className="fw-bold">Technologies: {project.technologies}</li>
-            <li>Key takeaways: {project.takeaways}</li>
-            {project.otherContributors?<li>{project.otherContributors}</li>: <></>}
-            <li><a target="_blank" rel="noopener noreferrer" href={project.repositoryLink}>GitHub Project</a></li>
-          </ul>
-          {project.deploymentLink
-            ?<a target="_blank" rel="noopener noreferrer" href={project.deploymentLink}>
-              <button className="btn btn-lg btn-dark m-3 shadow">
+    <div className="card mb-2 m-5 shadow">
+      <div className="card-body">
+        <h5>{project.description}</h5>
+        <ul>
+          <li className="fw-bold">Technologies: {project.technologies}</li>
+          <li>Key takeaways: {project.takeaways}</li>
+          {project.otherContributors && <li>{project.otherContributors}</li>}
+          <li><a target="_blank" rel="noopener noreferrer" href={project.repositoryLink}>GitHub Project</a></li>
+        </ul>
+        { project.deploymentLink && <a target="_blank" rel="noopener noreferrer" href={project.deploymentLink}>
+          <button className="btn btn-lg btn-dark m-3 shadow">
               Launch Live App
-              </button>
-            </a>
-            :<></>
-          }
-        </div>
+          </button>
+        </a>}
       </div>
-    </>
+    </div>
   );
 };
 
--- src/pages/ProjectDetails/ProjectDetails.jsx
+++ src/pages/ProjectDetails/ProjectDetails.jsx
@@ -13,25 +13,15 @@ const ProjectDetails = () => {
       <div className="column-centered-content">
         <h1> {project.title} </h1>
       </div>
-
       <ProjectDetailCard project={project} />
       <div className="m-5">
-        <a target="_blank" rel="noopener noreferrer" href={project.deploymentLink}>
-          <img src={project.image} alt={project.title} className="img-fluid shadow"/>
-        </a>
+        <img src={project.image} alt={project.title} className="img-fluid shadow"/>
         <h5>Screen image from app</h5>
       </div>
-      {project.image2 
-        ? <>
-          <div className="m-5">
-            <a target="_blank" rel="noopener noreferrer" href={project.deploymentLink}>
-              <img src={project.image2} alt={project.title} className="img-fluid shadow"/> 
-            </a>
-            <h5>Screen image from app</h5>
-          </div>
-        </>
-        : <></>
-      }
+      { project.image2 && <div className="m-5">
+        <img src={project.image2} alt={project.title} className="img-fluid shadow"/> 
+        <h5>Screen image from app</h5>
+      </div> }
       <div className="column-centered-content">
         <a href="/projects">
           <button className="btn btn-lg btn-outline-dark mb-3 mt-3 shadow">Back to Projects</button>
